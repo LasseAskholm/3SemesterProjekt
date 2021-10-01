120ef2c16afbff42943afc0ec94aa7ab420ed6d3
Akselthyregod <59265356+Akselthyregod@users.noreply.github.com>
2021-10-01 12:03:09 +0200

    Init Java setup with milo

---
+++ 3SemesterProject/src/main/java/test.java
@@ -0,0 +1,15 @@
+import org.eclipse.milo.opcua.sdk.client.api.config.OpcUaClientConfigBuilder;
+
+public class test {
+
+
+    public static void main(String[] args) {
+
+
+        OpcUaClientConfigBuilder cfg = new OpcUaClientConfigBuilder();
+
+        System.out.println("test");
+    }
+
+
+}
