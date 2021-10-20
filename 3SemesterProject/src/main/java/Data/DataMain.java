22b82dc35c1a443cd7071591e5e547533c0e6fcd
Aksel Thyregod Jensen <akselthyregod@live.dk>
2021-10-20 13:18:17 +0200

    Added localhost and connection to laravel

--- 3SemesterProject/src/main/java/Data/DataMain.java
+++ 3SemesterProject/src/main/java/Data/DataMain.java
@@ -9,30 +9,44 @@ public class DataMain {
     // singeltion instance
     private static DataMain instance;
 
-    // database setup
+
+    // database setup online
+    /*
     private String url = "hattie.db.elephantsql.com";
     private int port = 5432;
     private String databaseName = "rneumnoj";
     private String username = "rneumnoj";
     private String password = "d8DQNzagLUSxnbAE-W709BxmR86KKTj-";
+    */
 
-    private Connection connection = null;
+    // localhost
+    private String url = "localhost";
+    private int port = 3306;
+    private String databaseName = "db";
+    private String username = "root";
+    private String password = "secret";
 
-    private DataMain() {
+    private static Connection connection = null;
+
+    private DataMain() throws SQLException {
         initializePostgresqlDatabase();
     }
 
-    public static DataMain getInstance() {
+    public static DataMain getInstance() throws SQLException {
         if (instance == null) {
             instance = new DataMain();
         }
         return instance;
     }
 
-    private void initializePostgresqlDatabase() {
+    private void initializePostgresqlDatabase() throws SQLException {
         try {
-            DriverManager.registerDriver(new org.postgresql.Driver());
-            connection = DriverManager.getConnection("jdbc:postgresql://" + url + ":" + port + "/" + databaseName, username, password);
+            //DriverManager.registerDriver(new org.postgresql.Driver());
+            //mysql - Localhost
+            connection = DriverManager.getConnection("jdbc:mysql://" + url + ":" + port + "/" + databaseName, username, password);
+
+            //postgres - Online
+            //connection = DriverManager.getConnection("jdbc:postgresql://" + url + ":" + port + "/" + databaseName, username, password);
         } catch (SQLException | IllegalArgumentException ex) {
             ex.printStackTrace(System.err);
         } finally {
@@ -40,9 +54,30 @@ public class DataMain {
         }
     }
 
-    public static void main(String[] args) {
+    private static void testInsert(String s) throws SQLException {
+
+
+        try{
+            connection.setAutoCommit(false);
+
+            String test = "INSERT INTO java_tests(tests) values(\"test1\")";
+            PreparedStatement prep = connection.prepareStatement(test);
+            prep.execute();
+            prep.close();
+
+            connection.commit();
+        }catch (Exception e){
+            System.out.println("caught");
+            e.printStackTrace();
+        }
+
+
+    }
+    public static void main(String[] args) throws SQLException {
+
         DataMain db = DataMain.getInstance();
 
+        testInsert("s");
 
 
     }
